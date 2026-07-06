e35fef211d1a03b570082c6b668f2783bd9fc608
BAD <3589206993@qq.com>
2026-07-06 15:38:58 +0800

    更新

--- openwire/src-tauri/gen/android/app/src/main/java/com/openwire/app/MainActivity.kt
+++ openwire/src-tauri/gen/android/app/src/main/java/com/openwire/app/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.openwire.app
 
+import android.content.Context
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 
 class MainActivity : TauriActivity() {
+  private external fun initNdkContext(context: Context)
+
   override fun onCreate(savedInstanceState: Bundle?) {
     enableEdgeToEdge()
     super.onCreate(savedInstanceState)
+    try {
+      initNdkContext(applicationContext)
+    } catch (e: UnsatisfiedLinkError) {
+      android.util.Log.e("OpenWire", "JNI initNdkContext not found", e)
+    } catch (e: Exception) {
+      android.util.Log.e("OpenWire", "Failed to init NDK context", e)
+    }
   }
 }
